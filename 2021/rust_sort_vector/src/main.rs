6d8baa1c8e1531383c5d744536a8281c3557c44f
Sylvain Kerkour <sylvain@kerkour.com>
2021-08-22 20:00:12 +0000

    m

--- 2021/rust_sort_vector/src/main.rs
+++ 2021/rust_sort_vector/src/main.rs
@@ -1,3 +1,33 @@
+use std::cmp::Ordering;
+
+#[derive(Debug)]
+struct Point {
+    x: i64,
+    y: i64,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // sort
+    let mut v = vec![3, 2, 90, 78, 64, 32, 1, -10, 10, 10000];
+    v.sort();
+    println!("{:?}", v);
+
+    // sort_by
+    let mut v = vec![
+        Point { x: 3, y: 2 },
+        Point { x: 90, y: 78 },
+        Point { x: 64, y: 32 },
+        Point { x: 1, y: -10 },
+        Point { x: 10, y: 10000 },
+    ];
+    v.sort_by(|a, b| {
+        if a.x < b.x {
+            Ordering::Less
+        } else if a.x == b.x {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
+    });
+    println!("{:?}", v);
 }
