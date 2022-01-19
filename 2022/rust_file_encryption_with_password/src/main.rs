0585fc860c3802b69a101c4c7fbdee05c5c149dd
Sylvain Kerkour <sylvain@kerkour.com>
2022-01-19 18:53:23 +0000

    rust_file_encryption_with_password

--- 2022/rust_file_encryption_with_password/src/main.rs
+++ 2022/rust_file_encryption_with_password/src/main.rs
@@ -8,6 +8,7 @@ use std::{
     env,
     fs::File,
     io::{Read, Write},
+    time::Instant,
 };
 use zeroize::Zeroize;
 
@@ -36,12 +37,12 @@ fn main() -> Result<(), anyhow::Error> {
 }
 
 fn argon2_config<'a>() -> argon2::Config<'a> {
-    return argon2::Config{
+    return argon2::Config {
         variant: argon2::Variant::Argon2id,
         hash_length: 32,
         lanes: 8,
         mem_cost: 4 * 4096,
-        time_cost: 12,
+        time_cost: 8,
         ..Default::default()
     };
 }
