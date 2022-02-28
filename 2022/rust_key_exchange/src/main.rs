19fc042beed393e2f7c76a253f1734f9d617fb25
Sylvain Kerkour <sylvain@kerkour.com>
2022-02-28 10:09:16 +0000

    rust_key_exchange

---
+++ 2022/rust_key_exchange/src/main.rs
@@ -0,0 +1,47 @@
+use blake2::digest::{Update, VariableOutput};
+use rand::rngs::OsRng;
+use x25519_dalek::{EphemeralSecret, PublicKey, StaticSecret};
+
+pub const X25519_PRIVATE_KEY_SIZE: usize = 32;
+pub const X25519_PUBLIC_KEY_SIZE: usize = 32;
+pub const X25519_SECRET_SIZE: usize = 32;
+pub const XCHACHA20_POLY1305_KEY_SIZE: usize = 32;
+pub const STATIC_DATA: &str = "rust_key_exchange";
+
+fn main() {
+    let alice_private_key = StaticSecret::new(OsRng);
+    let alice_public_key = PublicKey::from(&alice_private_key);
+
+    let bob_private_key = StaticSecret::new(OsRng);
+    let bob_public_key = PublicKey::from(&bob_private_key);
+
+    let bob_secret = derive_secret_for_bob(&bob_private_key, &alice_public_key);
+    let alice_secret = derive_secret_for_bob(&bob_private_key, &alice_public_key);
+
+    assert!(bob_secret == alice_secret);
+}
+
+fn derive_secret_for_bob(bob_private_key: &StaticSecret, alice_public_key: &PublicKey) -> Vec<u8> {
+    let dh_secret = bob_private_key.diffie_hellman(&alice_public_key);
+
+    let mut encryption_kdf =
+        blake2::VarBlake2b::new_keyed(dh_secret.as_bytes(), XCHACHA20_POLY1305_KEY_SIZE);
+    encryption_kdf.update(STATIC_DATA.as_bytes());
+    let encryption_key = encryption_kdf.finalize_boxed();
+
+    return encryption_key.into();
+}
+
+fn derive_secret_for_alice(
+    alice_private_key: &StaticSecret,
+    bob_public_key: &PublicKey,
+) -> Vec<u8> {
+    let dh_secret = alice_private_key.diffie_hellman(&bob_public_key);
+
+    let mut encryption_kdf =
+        blake2::VarBlake2b::new_keyed(dh_secret.as_bytes(), XCHACHA20_POLY1305_KEY_SIZE);
+    encryption_kdf.update(STATIC_DATA.as_bytes());
+    let encryption_key = encryption_kdf.finalize_boxed();
+
+    return encryption_key.into();
+}
