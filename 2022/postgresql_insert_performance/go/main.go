2dad550631510347c27a247bbdade122ee81dc4e
Sylvain Kerkour <sylvain@kerkour.com>
2022-06-05 17:48:33 +0000

    2022/postgresql_insert_performance

--- 2022/postgresql_insert_performance/go/main.go
+++ 2022/postgresql_insert_performance/go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 
@@ -9,24 +10,73 @@ import (
 )
 
 func main() {
+	ctx := context.Background()
 	databaseUrl := os.Getenv("DATABASE_URL")
 	if databaseUrl == "" {
 		log.Fatal("env var DATABASE_URL is missing")
 	}
 
-	ctx := context.Background()
+	pool, err := dbConnect(ctx, databaseUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	defer pool.Close()
+
+}
+
+func dbConnect(ctx context.Context, databaseUrl string) (pool *pgxpool.Pool, err error) {
 	config, err := pgxpool.ParseConfig(databaseUrl)
 	if err != nil {
-		log.Fatalf("parsing database URL: %v", err)
+		err = fmt.Errorf("parsing database URL: %v", err)
+		return
 	}
 
 	config.MaxConns = 100
 
-	dbpool, err := pgxpool.ConnectConfig(ctx, config)
+	pool, err = pgxpool.ConnectConfig(ctx, config)
 	if err != nil {
-		log.Fatalf("Unable to connect to database: %v", err)
+		err = fmt.Errorf("Unable to connect to database: %v", err)
 	}
-	defer dbpool.Close()
 
+	return
+}
+
+func dbSetup(ctx context.Context, pool *pgxpool.Pool) (err error) {
+	query := `
+	CREATE EXTENSION IF NOT EXISTS timescaledb;
+
+    CREATE TABLE IF NOT EXISTS normalized (
+        id UUID PRIMARY KEY,
+        type TEXT NOT NULL,
+        timestamp TIMESTAMPTZ NOT NULL,
+        received_at TIMESTAMPTZ NOT NULL,
+        payload JSONB NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS key_value (
+        key UUID PRIMARY KEY,
+        value BYTEA NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS key_value_compressed (
+        key UUID PRIMARY KEY,
+        value BYTEA NOT NULL
+    );
+
+    CREATE TABLE IF NOT EXISTS timeseries (
+        timestamp TIMESTAMPTZ NOT NULL,
+        value BYTEA NOT NULL
+    );
+    CREATE INDEX index_timeseries_on_timestamp ON timeseries (timestamp);
+
+
+    CREATE TABLE IF NOT EXISTS timeseries_timescale (
+        timestamp TIMESTAMPTZ NOT NULL,
+        value BYTEA NOT NULL
+    );
+    SELECT create_hypertable('timeseries_timescale','timestamp');
+`
+	_, err = pool.Exec(ctx, query)
+	return
 }
