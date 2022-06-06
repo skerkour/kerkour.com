c598d65262d53c8e812e9b78c48bffd81f30121a
Sylvain Kerkour <sylvain@kerkour.com>
2022-06-06 09:57:46 +0000

    2022/postgresql_insert_performance

--- 2022/postgresql_insert_performance/rust/src/main.rs
+++ 2022/postgresql_insert_performance/rust/src/main.rs
@@ -11,7 +11,7 @@ use std::time::{Duration, Instant};
 use uuid::Uuid;
 
 const CONCURRENCY: u32 = 100;
-const EXECUTIONS: u64 = 1_000;
+const EXECUTIONS: u64 = 50_000;
 const RUNS: usize = 10;
 
 #[derive(sqlx::FromRow, Serialize, Debug, Clone, Deserialize)]
