2471a15785e36f2acb182f67c2831d2f0e2f7451
Sylvain Kerkour <sylvain@kerkour.com>
2022-06-07 08:14:02 +0000

    2022/postgresql_insert_performance

--- 2022/postgresql_insert_performance/go/main.go
+++ 2022/postgresql_insert_performance/go/main.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
@@ -124,9 +126,9 @@ func generateEvent() Event {
 	now := time.Now().UTC()
 
 	payload := Payload{
-		Something:      "",
-		SomethingElse:  []byte{},
-		SomethingElse2: []uint32{},
+		Something:      RandString(500),
+		SomethingElse:  make([]byte, 500),
+		SomethingElse2: make([]uint32, 500),
 	}
 
 	return Event{
@@ -136,4 +138,31 @@ func generateEvent() Event {
 		ReceivedAt: now,
 		Payload:    payload,
 	}
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandString(n int) string {
+	var src = rand.NewSource(time.Now().UnixNano())
+	sb := strings.Builder{}
+	sb.Grow(n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			sb.WriteByte(letterBytes[idx])
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return sb.String()
 }
